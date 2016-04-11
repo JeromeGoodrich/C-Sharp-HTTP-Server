c33486ce7edc282d73079d6c3d23424212c12480
Jgoodrich07 <jeromegoodrich@gmail.com>
2016-04-11 11:40:24 -0500

    pass TestArgsProvided

--- HTTPServerTest/ServerConfig.cs
+++ HTTPServerTest/ServerConfig.cs
@@ -1,19 +1,44 @@
 ﻿using System;
+using System.Linq;
 
 namespace HTTPServerTest
 {
     internal class ServerConfig
-    { 
+    {
+        private int _port;
+        private string _publicDir;
+
         public void SetUp(params string[] args) {
-            
+            SetPort(args);
+            SetPublicDir(args);
+        }
+
+        private void SetPort(params string[] args) {
+            if (args.Contains("-p")) {
+                var portIndex = Array.IndexOf(args, "-p") + 1;
+                _port = args[portIndex] != null ? int.Parse(args[portIndex]) : 5000;
+            }
+            else {
+                _port = 5000;
+            }
+        }
+
+        private void SetPublicDir(params string[] args) {
+            if (args.Contains("-d")) {
+                var dirIndex = Array.IndexOf(args, "-d") + 1;
+                _publicDir = args[dirIndex] ?? "./public";
+            }
+            else {
+                _publicDir = "./public";
+            }
         }
 
         public int GetPort() {
-            return 5000;
+            return _port;
         }
 
         public string GetPublicDir() {
-            return "./public";
+            return _publicDir;
         }
     }
 }
--- HTTPServerTest/UnitTest1.cs
+++ HTTPServerTest/UnitTest1.cs
@@ -8,9 +8,19 @@ namespace HTTPServerTest
         [Fact]
         public void TestDefaults() {
             var config = new ServerConfig();
-            config.SetUp("");
+            var args = new[] { "" };
+            config.SetUp(args);
             Assert.Equal(config.GetPort(), 5000);
             Assert.Equal(config.GetPublicDir(), "./public");
+        }
+        [Fact]
+        public void TestArgsProvided()
+        {
+            var config = new ServerConfig();
+            var args = new[] {"-p", "7000", "-d", "/this/directory"};
+            config.SetUp(args);
+            Assert.Equal(config.GetPort(), 7000);
+            Assert.Equal(config.GetPublicDir(), "/this/directory");
         }
     }
 }

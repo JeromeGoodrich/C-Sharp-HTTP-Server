23a47bd930ee9334632b6a02fa7a61bbac846dd9
Jgoodrich07 <jeromegoodrich@gmail.com>
2016-04-11 09:57:47 -0500

    first commit

---
+++ HTTPServerTest/ServerConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HTTPServerTest
+{
+    internal class ServerConfig
+    { 
+        public void SetUp(params string[] args) {
+            
+        }
+
+        public int GetPort() {
+            return 5000;
+        }
+
+        public string GetPublicDir() {
+            return "./public";
+        }
+    }
+}
---
+++ HTTPServerTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace HTTPServerTest
+{
+    
+    public class ServerConfigTest {
+        [Fact]
+        public void TestDefaults() {
+            var config = new ServerConfig();
+            config.SetUp("");
+            Assert.Equal(config.GetPort(), 5000);
+            Assert.Equal(config.GetPublicDir(), "./public");
+        }
+    }
+}

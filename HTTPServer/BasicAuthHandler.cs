5f4feb2a6b2f856fbe535949603b38cd1c51ea78
Jgoodrich07 <jeromegoodrich@gmail.com>
2016-04-20 23:24:09 -0500

    basic auth handler returns a status code of 200 when given credentials

--- HTTPServer/BasicAuthHandler.cs
+++ HTTPServer/BasicAuthHandler.cs
@@ -1,8 +1,13 @@
 ﻿namespace HTTPServer {
     public class BasicAuthHandler {
         public IResponse Handle(Request request) {
-            var response = new Response(401, request.Version);
-            response.AddHeader("WWW-Authenticate", "Basic realm=\"Camelot\"");
+            Response response;
+            if (request.GetHeaders().ContainsKey("Authorization")) {
+                response = new Response(200, request.Version);
+            } else {
+                response = new Response(401, request.Version);
+                response.AddHeader("WWW-Authenticate", "Basic realm=\"Camelot\"");
+            }
             return response;
         } 
     }
--- HTTPServerTest/BasicAuthHandlerTest.cs
+++ HTTPServerTest/BasicAuthHandlerTest.cs
@@ -6,32 +6,45 @@ namespace HTTPServerTest {
     public class BasicAuthHandlerTest {
 
         private IResponse _response;
+        private Request _request;
+        private BasicAuthHandler _handler;
 
         public BasicAuthHandlerTest() {
-            var request = new Request()
+            _request = new Request()
             {
                 Method = "GET",
                 Path = "/logs",
                 Version = "HTTP/1.1"
             };
-            var handler = new BasicAuthHandler();
+            _handler = new BasicAuthHandler();
 
-            _response = handler.Handle(request);
+            
         }
 
         [Fact]
-        public void Returns401Test() {
+        public void Returns401WithoutCredentialsTest() {
+            _response = _handler.Handle(_request);
             Assert.Equal(401, _response.StatusCode);
         }
 
         [Fact]
         public void RequiresAuthenticationTest() {
+            _response =_handler.Handle(_request);
             Assert.Equal("Basic realm=\"Camelot\"", _response.GetHeader("WWW-Authenticate"));
         }
 
         [Fact]
         public void BodyHasNoContentTest() {
+            _response = _handler.Handle(_request);
             Assert.Equal(null, _response.Body);
+        }
+
+        [Fact]
+        public void Returns200WithCredentialsTest() {
+            _request.AddHeader("Authorization", "Basic YWRtaW46aHVudGVyMg==");
+            _response = _handler.Handle(_request);
+
+            Assert.Equal(200, _response.StatusCode);
         }
 
 

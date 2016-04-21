248e7fc5d842fdd840dc803de784733185e2811f
Jgoodrich07 <jeromegoodrich@gmail.com>
2016-04-21 09:12:57 -0500

    Handle Pdfs big and small

--- HTTPServer/FileHandler.cs
+++ HTTPServer/FileHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Web;
 
 namespace HTTPServer {
     public class FileHandler : IHandler {
@@ -10,9 +11,20 @@ namespace HTTPServer {
 
         public IResponse Handle(Request request) {
             var response = new Response(200, "HTTP/1.1");
+            var filePath = _publicDir + request.Path;
+            var extension = Path.GetExtension(filePath);
+            string mimeType;
+            if (!extension.Equals("")) {
+                mimeType = MimeMapping.GetMimeMapping(filePath);
+            } else {
+                mimeType = "text/plain";
+            }
             var fileBytes = File.ReadAllBytes(_publicDir + request.Path);
             response.AddHeader("Content-Length", fileBytes.Length.ToString());
-            response.AddHeader("Content-Type", "text/plain");
+            response.AddHeader("Content-Type", mimeType);
+            if (mimeType.Equals("application/pdf") && fileBytes.Length > 10485760) {
+                response.AddHeader("Content-Disposition", "attachment; filename=\"big-pdf.pdf\"");
+            }
             response.Body = fileBytes;
             return response;
         }
--- HTTPServerTest/FileHandlerTest.cs
+++ HTTPServerTest/FileHandlerTest.cs
@@ -15,39 +15,39 @@ namespace HTTPServerTest {
             _request = new Request()
             {
                 Method = "GET",
-                Path = "/file1",
                 Version = "HTTP/1.1."
             };
             _handler = new FileHandler(_publicDir);
         }
 
         [Fact]
-        public void WillHandleTest() { 
+        public void WillHandleTest() {
+            _request.Path = "/file1";
+
             Assert.True(_handler.WillHandle(_request.Method, _request.Path));
         }
 
         [Fact]
         public void WillNotHandleTest() {
-            var badRequest = new Request()
-            {
-                Method = "GET",
-                Path = "/foobar",
-                Version = "HTTP/1.1."
-            };
+            _request.Path = "/badpath";
             var handler = new FileHandler(_publicDir);
-            Assert.False(handler.WillHandle(badRequest.Method, badRequest.Path));
+
+            Assert.False(handler.WillHandle(_request.Method, _request.Path));
         }
 
         [Fact]
-        public void RequestForTextReturns200() {
+        public void RequestForFileReturns200Test() {
+            _request.Path = "/file1";
             var response = _handler.Handle(_request);
+
             Assert.Equal(200, response.StatusCode);
         }
 
         [Fact]
-        public void RequestForTextReturnsCorrectHeaders()
-        {
+        public void RequestForTextReturnsCorrectHeadersTest() { 
+            _request.Path = "/file1";
             var response = _handler.Handle(_request);
+
             Assert.Equal("14", response.GetHeader("Content-Length"));
             Assert.Equal("text/plain", response.GetHeader("Content-Type"));
         }
@@ -55,25 +55,47 @@ namespace HTTPServerTest {
 
         [Fact]
         public void ContentofTextFileIsInResponseBodyTest() {
+            _request.Path = "/file1";
             var response = _handler.Handle(_request);
+
             Assert.Equal("file1 contents", Encoding.UTF8.GetString(response.Body));
         }
 
-    
+        [Fact]
+        public void RequestForImageReturnsCorrectHeadersTest() { 
+            _request.Path = "/image.jpeg";
+            var response = _handler.Handle(_request);
+
+            Assert.Equal("157751", response.GetHeader("Content-Length"));
+            Assert.Equal("image/jpeg", response.GetHeader("Content-Type"));
+        }
 
         [Fact]
-        public void ContentofImageIsInResponseBodyTest()
+        public void RequestForImageHasResponseBodyTest()
         {
-            var request = new Request()
-            {
-                Method = "GET",
-                Path = "/file1",
-                Version = "HTTP/1.1."
-            };
-            var handler = new FileHandler(_publicDir);
-            var response = handler.Handle(request);
-            Assert.Equal("file1 contents", Encoding.UTF8.GetString(response.Body));
+            _request.Path = "/image.jpeg";
+            var response = _handler.Handle(_request);
 
+            Assert.NotNull(Encoding.UTF8.GetString(response.Body));
+        }
+
+        [Fact]
+        public void RequestForSmallPdfReturnsCorrectHeadersTest() {
+            _request.Path = "/pdf-sample.pdf";
+            var response = _handler.Handle(_request);
+
+            Assert.Equal("7945", response.GetHeader("Content-Length"));
+            Assert.Equal("application/pdf", response.GetHeader("Content-Type"));
+        }
+
+        [Fact]
+        public void RequestForBigPdfReturnsCorrectHeadersTest() {
+            _request.Path = "/big-pdf.pdf";
+            var response = _handler.Handle(_request);
+
+            Assert.Equal("10762150", response.GetHeader("Content-Length"));
+            Assert.Equal("application/pdf", response.GetHeader("Content-Type"));
+            Assert.Contains("attachment;", response.GetHeader("Content-Disposition"));
         }
     }
 }

6197c22c01fd70ef2a8dd66eb36044d878644eef
Jgoodrich07 <jeromegoodrich@gmail.com>
2016-04-26 16:11:47 -0500

    refactor parser test to incorporate URL params

--- HTTPServerTest/ParserTest.cs
+++ HTTPServerTest/ParserTest.cs
@@ -8,7 +8,10 @@ namespace HTTPServerTest {
         private readonly Request _request;
 
         public ParserTest() {
-            var rawRequest = Encoding.UTF8.GetBytes("POST /form HTTP/1.1\r\n" +
+            var rawRequest = Encoding.UTF8.GetBytes("GET /parameters?variable_1=Operators%20%3C%2C%20%" +
+                                                    "3E%2C%20%3D%2C%20!%3D%3B%20%2B%2C%20-%2C%20*%2C%20%26%2" +
+                                                    "C%20%40%2C%20%23%2C%20%24%2C%20%5B%2C%20%5D%3A%20%22is%20that" +
+                                                    "%20all%22%3F&variable_2=stuff HTTP/1.1\r\n" +
                                                     "Host: www.example.com\r\n" +
                                                     "Content-Length: 34\r\n\r\n" +
                                                     "firstname=jerome&lastname=goodrich");
@@ -20,12 +23,12 @@ namespace HTTPServerTest {
 
         [Fact]
         public void TestParseMethod() {
-            Assert.Equal("POST", _request.Method);
+            Assert.Equal("GET", _request.Method);
         }
 
         [Fact]
         public void TestParsePath() {
-            Assert.Equal("/form", _request.Path);
+            Assert.Equal("/parameters", _request.Path);
         }
 
         [Fact]
@@ -46,19 +49,8 @@ namespace HTTPServerTest {
 
         [Fact]
         public void TestParseParams() {
-            var paramsRequest =
-                Encoding.UTF8.GetBytes(
-                    "GET " +
-                    "/parameters?variable_1=Operators%20%3C%2C%20%3E%2C%20%3D%2C%20!%3D%3B%20%2B%2C%" +
-                    "20-%2C%20*%2C%20%26%2C%20%40%2C%20%23%2C%20%24%2C%20%5B%2C%20%5D%3A%20%22is%20that%20all%22%3F&variable_2=stuff " +
-                    "HTTP/1.1\r\n" +
-                    "Host: www.example.com\r\n\r\n");
-            var stream = new MemoryStream(paramsRequest);
-            var parser = new Parser();
-            var request = parser.Parse(new StreamReader(stream));
-
-            Assert.True(request.GetParameters().ContainsKey("variable_1"));
-            Assert.True(request.GetParameters().ContainsValue("stuff"));
+            Assert.True(_request.GetParameters().ContainsKey("variable_1"));
+            Assert.True(_request.GetParameters().ContainsValue("stuff"));
 
         }
     }

0b3d5d0c11e8ad775215a06fd13ff27c1262b8ba
Jgoodrich07 <jeromegoodrich@gmail.com>
2016-04-26 20:31:01 -0500

    Form Handler Handles Post and Get Requests

--- HTTPServer/FormDataHandler.cs
+++ HTTPServer/FormDataHandler.cs
@@ -1,6 +1,17 @@
-﻿namespace HTTPServer {
+﻿using System.Text;
+
+namespace HTTPServer {
     public class FormDataHandler : IHandler {
+        private byte[] _formData;
+
         public IResponse Handle(Request request) {
+            if (request.Method.Equals("GET")) {
+                var response = new Response(200, request.Version) {
+                    Body = _formData
+                };
+                return response;
+            }
+            _formData = Encoding.UTF8.GetBytes(request.Body);
             return new Response(200, request.Version);
         }
     }
--- HTTPServerTest/FormDataHandlerTest.cs
+++ HTTPServerTest/FormDataHandlerTest.cs
@@ -1,19 +1,28 @@
-﻿using HTTPServer;
+﻿using System.Text;
+using HTTPServer;
 using Xunit;
 
 namespace HTTPServerTest {
     public class FormDataHandlerTest {
         [Fact]
-        public void GetRequestToFormReturns200() {
-            var request = new Request() {
+        public void GetAfterPostHasBodyContent() {
+            var postRequest = new Request {
+                Method = "POST",
+                Path = "/form",
+                Version = "HTTP/1.1",
+                Body = "data=fatcat"
+            };
+            var handler = new FormDataHandler();
+            handler.Handle(postRequest);
+            var getRequest = new Request {
                 Method = "GET",
                 Path = "/form",
                 Version = "HTTP/1.1"
             };
-            var handler = new FormDataHandler();
-            var response = handler.Handle(request);
+            var response = handler.Handle(getRequest);
 
             Assert.Equal(200, response.StatusCode);
+            Assert.Equal("data=fatcat", Encoding.UTF8.GetString(response.Body));
         } 
     }
 }

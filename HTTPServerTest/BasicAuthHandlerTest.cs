377155af4447d5910c1f78671fa7c653ae7e3de2
Jgoodrich07 <jeromegoodrich@gmail.com>
2016-04-28 09:49:44 -0500

    refactor test spacing in Basic Auth Test

--- HTTPServerTest/BasicAuthHandlerTest.cs
+++ HTTPServerTest/BasicAuthHandlerTest.cs
@@ -21,18 +21,21 @@ namespace HTTPServerTest {
         [Fact]
         public void Returns401WithoutCredentialsTest() {
             _response = _handler.Handle(_request);
+
             Assert.Equal(401, _response.StatusCode);
         }
 
         [Fact]
         public void RequiresAuthenticationTest() {
             _response = _handler.Handle(_request);
+
             Assert.Equal("Basic realm=\"Camelot\"", _response.GetHeader("WWW-Authenticate"));
         }
 
         [Fact]
         public void BodyHasNoContentTest() {
             _response = _handler.Handle(_request);
+
             Assert.Equal(null, _response.Body);
         }
 

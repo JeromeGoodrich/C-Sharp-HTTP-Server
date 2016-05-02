d21ab63c64cbd36d10b4580a8e2102425433a673
Jgoodrich07 <jeromegoodrich@gmail.com>
2016-05-02 10:30:14 -0500

    add xunit plugin for Visual Studio

---
+++ HTTPServerTest/Status.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace HTTPServer {
+    public class Status {
+        public static Dictionary<int, string> StatusDictionary
+            = new Dictionary<int, string> {
+                {200, "OK"},
+                {204, "No Content"},
+                {206, "Partial Content"},
+                {302, "Found" },
+                {401, "Unauthorized"},
+                {404, "Not Found"},
+                {405, "Method Not Allowed" }
+            };
+    }
+}

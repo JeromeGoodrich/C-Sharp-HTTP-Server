b7100aa3a6bbea4ff066d77d55aa8dc8693923e6
Jgoodrich07 <jeromegoodrich@gmail.com>
2016-05-06 08:22:46 -0500

    add basic form handler

---
+++ BasicServer/BasicFormHandler.cs
@@ -0,0 +1,40 @@
+﻿using ServerClassLibrary;
+using System.Text;
+
+namespace BasicServer {
+
+   public class BasicFormHandler : IHandler {
+
+        private static string _formData;
+
+        public IResponse Handle(Request request) {
+            if (request.Method.Equals("GET")) {
+                return HandleGet(request);
+            } else {
+                return HandlePost(request);
+            }
+        }
+
+    private Response HandleGet(Request request) {
+        string HTMLBoilerPlate = "<!DOCTYPE html>\n<html>\n<header>\n</header>\n<body>\n";
+        string openFormTag = "<form method=\"post\">";
+        string field = "First Name:\n<input type=\"text\" name=\"name\">\n";
+        string submit = "<input type=\"submit\" value=\"Submit\">\n</form>\n";
+        string endboilderplate = "</body>\n</html>";
+        string htmlContent;
+        if (_formData != null) {
+            htmlContent =  HTMLBoilerPlate + openFormTag + field  + submit + _formData.Split('=')[1] + endboilderplate;
+        } else {
+            htmlContent = HTMLBoilerPlate + openFormTag + field  + submit;
+        }
+        byte[] data = Encoding.UTF8.GetBytes(htmlContent);
+        return new Response(200, request.Version) { Body = data };
+    }
+
+    private Response HandlePost(Request request) {
+        _formData = request.Body;
+        return new Response(200, request.Version);
+    }
+
+    }
+}
---
+++ BasicServerTest/BasicFormHandlerTest.cs
@@ -0,0 +1,53 @@
+﻿using Xunit;
+using ServerClassLibrary;
+using BasicServer;
+using System.Text;
+
+namespace BasicServerTest {
+    public class BasicFormHandlerTest {
+
+        private Request _request = new Request() {
+            Method = "GET",
+            Path = "/form",
+            Version = "HTTP/1.1"
+        };
+        private IHandler _handler = new BasicFormHandler();
+        private IResponse _response;
+
+
+        [Fact]
+        public void GetReturns200() {
+            _response = _handler.Handle(_request);
+
+            Assert.Equal(200, _response.StatusCode);
+        }
+
+        [Fact]
+        public void PostReturns200() {
+            _request.Method = "POST";
+            _response = _handler.Handle(_request);
+
+            Assert.Equal(200, _response.StatusCode);
+        }
+
+        [Fact]
+        public void GetContainsHtmlForm() {
+            _response = _handler.Handle(_request);
+
+            Assert.Contains("<form method" , Encoding.UTF8.GetString(_response.Body));
+        }
+
+        [Fact]
+        public void GetContainsValueAfterPost() {
+            _request.Method = "POST";
+            _request.Body = "name=juice";
+            _handler.Handle(_request);
+
+            _request.Method = "GET";
+            _request.Body = null;
+            _response = _handler.Handle(_request);
+
+            Assert.Contains("juice", Encoding.UTF8.GetString(_response.Body));
+        }
+    }
+}
